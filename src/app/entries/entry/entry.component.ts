69acaaa93f0c786bd1588943983b4e5215ceab03
dennomwas <githinji.mwangi@gmail.com>
2018-09-18 22:49:02 +0300

    [Ft] add entry component

---
+++ src/app/entries/entry/entry.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-entry',
+    templateUrl: 'entry.component.html',
+    styleUrls: ['entry.component.css']
+})
+export class EntryComponent {
+    title: string = "My First Photo";
+    image: string = "http://placehold.it/800x500?text=Angular Basics";
+    description: string = "A description of my first photo";
+
+}
